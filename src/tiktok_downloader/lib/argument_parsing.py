94041bb4e633f82727107f71d6630168d770d4a9
Psyhackological <konradkon@duck.com>
2023-04-14 15:49:19 +0200

    Added short arguments

--- src/tiktok_downloader/lib/argument_parsing.py
+++ src/tiktok_downloader/lib/argument_parsing.py
@@ -13,16 +13,19 @@ def parse_arguments() -> Tuple[str, str, str]:
     """
     parser = argparse.ArgumentParser(description="TikTok Video Downloader")
     parser.add_argument(
+        "-i",
         "--input-file",
         default="tiktok_links.txt",
         help="Path to the input file containing TikTok video links",
     )
     parser.add_argument(
+        "-o",
         "--output-folder",
         default="TikToks",
         help="Path to the folder where downloaded videos will be saved",
     )
     parser.add_argument(
+        "-p",
         "--proxy-url",
         default="https://proxitok.pussthecat.org",
         help="The proxy URL to be used for downloading videos",
